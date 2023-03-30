2835952d9ac0bb54b29eaf7226ef0c19676b2a34
DGZCSXWD <fjjychenrui@gmail.com>
2023-03-30 18:55:09 +1100

    movie detail functional carousel

---
+++ assests/js/movie-details.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const film = JSON.parse(localStorage.getItem("selectedFilm"));
+
+  if (film) {
+    document.getElementById("movie-name").textContent = film.film_name;
+    document.getElementById("movie-poster").src =
+      film.images.poster["1"].medium.film_image;
+    document.getElementById("age-rating").textContent =
+      film.age_rating[0].rating + " - " + film.age_rating[0].age_advisory;
+    document.getElementById("synopsis").textContent = film.synopsis_long;
+    // document.getElementById("movie-trailer").querySelector("source").src =
+    //   film.film_trailer;
+  } else {
+    // Redirect back to the main page if no film data is found in localStorage
+    window.location.href = "index.html";
+  }
+});
--- assests/js/script.js
+++ assests/js/script.js
@@ -1,7 +1,5 @@
 "use strict";
 
-const filmsNowShowingUrl =
-  "https://api-gate2.movieglu.com/filmsNowShowing/?n=5";
 const filmsDetailsUrl =
   "https://api-gate2.movieglu.com/filmDetails/?film_id=7772"; //change the film id
 const filmsComingSoonUrl =
@@ -15,6 +13,8 @@ const cinemaDetailUrl =
 const cinemaShowtimeUrl =
   "https://api-gate2.movieglu.com/cinemaShowTimes/?film_id=7772&cinema_id=8930&date=2023-03-30"; //with or without film id
 const cinemaNearbyUrl = "https://api-gate2.movieglu.com/cinemasNearby/?n=2"; //change the n if want to show more
+const filmsNowShowingUrl =
+  "https://api-gate2.movieglu.com/filmsNowShowing/?n=5";
 
 function getCurrentDatetime() {
   return new Date().toISOString();
@@ -29,29 +29,85 @@ const headers = new Headers({
   territory: "XX",
 });
 
-const fetchData = function (url) {
-  fetch(url, {
-    method: "GET",
-    headers: headers,
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.error("There was a problem with the fetch operation:", error);
+async function fetchData(url) {
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: headers,
     });
-};
 
-fetchData(filmsNowShowingUrl);
-// fetchData(filmsDetailsUrl);
-// fetchData(filmsComingSoonUrl);
-// fetchData(filmsLiveSearchUrl);
-// fetchData(cinemaDetailUrl);
-// fetchData(cinemaLiveSarchUrl);
-// fetchData(cinemaShowtimeUrl);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+  }
+}
+
+function createCarouselItem(film) {
+  const carouselItem = document.createElement("a");
+  carouselItem.classList.add("carousel-item");
+  carouselItem.href = `#${film.film_id}`;
+
+  const img = document.createElement("img");
+  img.src = film.images.poster["1"].medium.film_image;
+  img.alt = film.film_name;
+
+  carouselItem.appendChild(img);
+  return carouselItem;
+}
+
+async function loadCarousel() {
+  const apiResponse = await fetchData(filmsNowShowingUrl);
+
+  if (apiResponse) {
+    // Add carousel items to the carousel
+    const carouselElement = document.getElementById("movie-carousel");
+    apiResponse.films.forEach((film) => {
+      const carouselItem = createCarouselItem(film);
+      carouselElement.appendChild(carouselItem);
+
+      carouselItem.addEventListener("click", () => {
+        localStorage.setItem("selectedFilm", JSON.stringify(film));
+        window.location.href = "/detailpage";
+      });
+    });
+
+    // Initialize the MaterializeCSS carousel
+    const carouselOptions = {
+      fullWidth: false,
+      indicators: true,
+    };
+    const carousel = document.querySelectorAll(".carousel");
+    const instances = M.Carousel.init(carousel, carouselOptions);
+
+    // Add event listeners for the carousel control buttons
+    document
+      .querySelector(".carousel-prev")
+      .addEventListener("click", carouselPrev);
+    document
+      .querySelector(".carousel-next")
+      .addEventListener("click", carouselNext);
+  }
+}
+
+// Function for moving the carousel to the previous item
+function carouselPrev() {
+  const carouselInstance = M.Carousel.getInstance(
+    document.querySelector(".carousel")
+  );
+  carouselInstance.prev();
+}
+
+// Function for moving the carousel to the next item
+function carouselNext() {
+  const carouselInstance = M.Carousel.getInstance(
+    document.querySelector(".carousel")
+  );
+  carouselInstance.next();
+}
+
+document.addEventListener("DOMContentLoaded", loadCarousel);
