251d60bdfcf2c7982c14c0b4e9b1acf00f3166ed
CGrace <c.grace5@yahoo.com.au>
2023-04-01 20:36:05 +1100

    Giga commit with moviesearch API, loads of functionality and Tristan magic backed up by CG confusion. Enjoy!

---
+++ assets/js/movie-search.js
@@ -0,0 +1,90 @@
+//var movie = '';
+var movieSessions = 'https://taskinoz.com/village/movies';
+var movies = [];
+//pulling from the API
+function getMoviesFromCinema(cinemaId) {
+
+    $('#all-movies').html('<div class="loading">loading...</div>')
+
+    $.ajax({
+        url: movieSessions + '?id=' + cinemaId,
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            movies = data;
+
+            $('#all-movies').text("")
+            var list = $('<div></div>').attr('id', 'movie-list'); //provides the detailed list of information about the movies, such as name, run time and genre.
+
+            for (let i = 0; i < movies.Items.length; i++) {
+                var movie = movies.Items[i];
+                var sessions = movie.Sessions;
+                var times = "";
+                console.log(times);
+                for (let j = 0; j < sessions.length; j++) {
+                    var session = sessions[j];
+                    times += '<span>' + dayjs(session.ShowDateTime).format("ddd hh:mm a") + '</span>\n';
+                }
+                var div = $(
+                    '<div class="movie">' +
+                    '<div class="movie-image">' +
+                    '<img src="' + movie.GraphicUrl + '">' +
+                    '</div>' +
+                    '<div class="movie-info">' +
+                    '<h3 class="movie-title">' + movie.Title + '</h3>' +
+                    '<p class="movie-rating">Rating: ' + movie.Rating + '</p>' +
+                    '<p class="movie-genre">' + movie.Genres[0].Name + '</p>' +
+                    '<p class="movie-duration">' + movie.Runtime + ' minutes</p>' +
+                    '<div class="movie-sessions">' +
+                    times +
+                    '</div>' +
+                    '</div>' +
+                    '</div>'
+                );
+                list.append(div);
+            }
+
+            $('#all-movies').append(list);
+        },
+        error: function (data) {
+            console.log(data);
+        }
+    });
+
+}
+
+function movieSearch() {
+    var search = $('#movie-search').val();
+    $('#movie-list .movie').each(function () {
+        if ($(this).children('.movie-info').children('.movie-title').text().toLowerCase().indexOf(search.toLowerCase()) == -1 && search !== "") {
+            $(this).hide();
+        } else {
+            $(this).show();
+        }
+    });
+}
+
+$(document).ready(function () {
+    // Get url parameters
+    var urlParams = new URLSearchParams(window.location.search);
+    var cinemaId = urlParams.get('cinemaId');
+    var cinemaName = urlParams.get('cinemaName');
+
+    if (cinemaName) {
+        $('h1').text(cinemaName + " Cinema");
+    }
+
+    if (cinemaId) {
+        getMoviesFromCinema(cinemaId)
+    }
+
+    $('#movie-search').keypress(function (e) {
+        if (e.which == 13) {//Enter key pressed
+            movieSearch();
+        }
+    });
+
+    $('button').on('click', function () {
+        movieSearch();
+    })
+});
