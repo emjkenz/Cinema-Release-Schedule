fc611c661d183ad86e4092b8ee84b0d85a13cee0
taskinoz <tristan@taskinoz.com>
2023-04-03 20:24:14 +1000

    Removed check on movie trailer

--- assets/js/movie-details.js
+++ assets/js/movie-details.js
@@ -8,14 +8,8 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("age-rating").textContent =
       film.age_rating[0].rating + " - " + film.age_rating[0].age_advisory;
     document.getElementById("synopsis").textContent = film.synopsis_long;
-    if (film.film_trailer) {
-      console.log(film.film_trailer);
-      document.getElementById("movie-trailer").querySelector("source").src =
-        film.film_trailer;
-    }
-    else {
-      document.getElementById("movie-trailer").remove();
-    }
+    document.getElementById("movie-trailer").querySelector("source").src =
+      film.film_trailer;
   } else {
     // Redirect back to the main page if no film data is found in localStorage
     window.location.href = "../";
