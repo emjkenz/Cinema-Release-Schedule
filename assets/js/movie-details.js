bc56d8500288602de4f4e80a093ccb814e90b3a8
taskinoz <tristan@taskinoz.com>
2023-04-03 20:20:58 +1000

    Added favicon

--- assets/js/movie-details.js
+++ assets/js/movie-details.js
@@ -8,8 +8,14 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("age-rating").textContent =
       film.age_rating[0].rating + " - " + film.age_rating[0].age_advisory;
     document.getElementById("synopsis").textContent = film.synopsis_long;
-    document.getElementById("movie-trailer").querySelector("source").src =
-      film.film_trailer;
+    if (film.film_trailer) {
+      console.log(film.film_trailer);
+      document.getElementById("movie-trailer").querySelector("source").src =
+        film.film_trailer;
+    }
+    else {
+      document.getElementById("movie-trailer").remove();
+    }
   } else {
     // Redirect back to the main page if no film data is found in localStorage
     window.location.href = "../";
